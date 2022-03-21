78a54e3fb87e11c2a3c458d96b33c0c1c75f8109
zkr024 <zkr.bm.02403@gmail.com>
2022-03-21 17:01:06 -0500

    'module' is not defined. (no-undef) error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,14 @@
-
-define(function (require, exports, module) {
-    module.exports = {
-        "env": {
-            "browser": true,
-            "es2021": true
-        },
-        "extends": "eslint:recommended",
-        "parserOptions": {
-            "ecmaVersion": "latest",
-            "sourceType": "module"
-        },
-        "rules": {
-        }
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
     }
-});
+}
