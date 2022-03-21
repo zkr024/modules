05df7870e8ba11b42ea8cd0379be003ea8917b63
zkr024 <zkr.bm.02403@gmail.com>
2022-03-21 16:33:52 -0500

    Linters - module declaration

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+
+define(function (require, exports, module) {
+    module.exports = {
+        "env": {
+            "browser": true,
+            "es2021": true
+        },
+        "extends": "eslint:recommended",
+        "parserOptions": {
+            "ecmaVersion": "latest",
+            "sourceType": "module"
+        },
+        "rules": {
+        }
+    }
+});
